99a9a31035dad9610a7a245e6af2fa395a6ff45d
Maxsilon1 <maxsilongusakov@gmail.com>
2025-12-07 14:12:17 +0300

    Fixed vector

--- Zad5/Zad5.cpp
+++ Zad5/Zad5.cpp
@@ -191,39 +191,98 @@ private:
 
 	using AllocTraits = std::allocator_traits<Alloc>;
 public:
-	vector() {}
-	vector(size_t sz): size_(sz) {}
-	vector(vector& other): arr_(std::move(other)) {}
-
+	vector(): arr_(nullptr), cap_(0), size_(0) {}
+	vector(size_t sz): arr_(AllocTraits::allocate(alloc_, sz)), cap_(sz), size_(sz) {}
+	vector(std::initializer_list<T> ilist)
+	{
+		reserve(ilist.size());
+		for (const T& i : ilist)
+			push_back(i);
+	}
+	explicit vector(const vector& other) : arr_(nullptr), cap_(), size_() 
+	{
+		resize(other.size_);
+		for (size_t i = 0; i < other.size_; ++i)
+			push_back(other.arr_[i]);
+	}
+	vector(vector&& other): arr_(other.arr_), cap_(other.cap_), size_(other.size_) 
+	{
+		other.arr_ = nullptr;
+		other.size_ = 0;
+		other.cap_ = 0;
+	}
 private:
 	template<bool isConst>
 	class base_iterator
 	{
 	private:
-		using reference_type = std::conditional<isConst, const T&, T&>;
-		using pointer_type = std::conditional<isConst, const T*, T*>;
+		using iterator_category = std::random_access_iterator_tag;
+		using reference = std::conditional_t<isConst, const T&, T&>;
+		using pointer = std::conditional_t<isConst, const T*, T*>;
 		using value_type = T;
+		using difference_type = std::ptrdiff_t;//любой целочисленный тип для смещения итераторов
 
 		T* ptr;
 	public:
+		base_iterator(T* p = nullptr): ptr(p) {}
+
 		base_iterator (const base_iterator&) = default;
 		base_iterator& operator=(const base_iterator& other) = default;
 
-		reference_type operator*() { return *ptr; }
-		reference_type operator->() {}
-		pointer_type operator&() { return ptr; }
+		reference operator*() { return *ptr; }
+		pointer operator->() const { return ptr; }
 
-		pointer_type operator++() { ++ptr; return *this; }
-		pointer_type operator++(int) { base_iterator old = *this; ++ptr; return old; }
-		pointer_type operator--() { return --ptr; }
-		pointer_type operator--(int) { base_iterator old = *this; ++ptr; return old; }
+		pointer operator&() { return ptr; }
 
+		base_iterator& operator++() { ++ptr; return *this; }
+		base_iterator operator++(int) { base_iterator old = *this; ++ptr; return old; }
+		
+		base_iterator& operator--() { return --ptr; }
+		base_iterator operator--(int) { base_iterator old = *this; --ptr; return old; }
 
+		base_iterator& operator+=(difference_type other)
+		{
+			ptr += other;
+			return *this;
+		}
+		base_iterator& operator-=(difference_type other)
+		{
+			ptr -= other;
+			return *this;
+		}
+
+		friend base_iterator operator+(base_iterator it, difference_type other)
+		{
+			return it += other;
+		}
+		friend base_iterator operator+(difference_type other, base_iterator it)
+		{
+			return it += other;
+		}
+		
+		friend base_iterator operator-(base_iterator it, difference_type other)
+		{
+			return it -= other;
+		}
+		friend base_iterator operator-(const base_iterator& it1, const base_iterator& it2)
+		{
+			return it1.ptr - it2.ptr;
+		}
+
+		bool operator==(const base_iterator& other) { return ptr == other.ptr; }
+		bool operator!=(const base_iterator& other) { return ptr != other.ptr; }
+
+		bool operator<(const base_iterator& other) { return *this < other; }
+		bool operator>(const base_iterator& other) { return *this > other; }
+
+		bool operator<=(const base_iterator& other) { return *this <= other; }
+		bool operator>=(const base_iterator& other) { return *this >= other; }
 	};
 	using iterator = base_iterator<false>;
 	using const_iterator = base_iterator<true>;
 public:
 
+	T& operator[](size_t i) const { return *(arr_ + i); }
 	vector& operator=(const vector& other) noexcept
 	{
 		Alloc newalloc_ = AllocTraits::propagate_on_container_copy_assigment::value ? other.alloc_ : alloc_;
@@ -253,66 +312,125 @@ public:
 		size_ = other.size_;
 		cap_ = other.cap_;
 	}
+	
+	vector* operator+=(const T& value)
+	{
+		for (size_t i = 0; i < size_; ++i)
+			arr_[i] += value;
+		return *this;
+	}
+	vector& operator+=(const vector& other) 
+	{
+		if (size_ != other.size_)throw std::length_error("bad sizes");
+		for (size_t i = 0; i < size_; ++i)
+			arr_[i] += other.arr_[i];
+		return *this;
+	}
 
-	void push_back(const T& tmp)
+	vector& operator-=(const T& value)
+	{
+		for (size_t i = 0; i < size_; ++i)
+			arr_[i] -= value;
+		return *this;
+	}
+	vector& operator-=(const vector& other) 
+	{
+		if (size_ != other)throw std::length_error("bad sizes");
+		for (size_t i = 0; i < size_; ++i)
+			arr_[i] -= other.arr_[i];
+		return *this;
+	}
+
+	friend vector& operator+(vector first, const vector& second)
+	{
+		return first += second;
+	}
+	friend vector& operator+(vector first, T& second)
+	{
+		return first += second;
+	}
+	friend vector& operator+(T& first, vector second) 
+	{
+		return second += first;
+	}
+
+	friend vector& operator-(vector first, const vector& second)
+	{
+		return first -= second;
+	}
+	friend vector& operator-(vector first, const T& second) { return first -= second; }
+	friend vector& operator-(const T& first, vector second) { return second -= first; }
+	
+	friend std::ostream& operator<<(std::ostream& os, const vector v)//У std::ostream - удален конструктор копирования, можно вызывать только по ссылке
+	{
+		os << "{";//Вводим в поток os все наши данные
+		for (size_t i = 0; i < v.size_; ++i)
+		{
+			os << v[i] << (i < v.size() - 1 ? " " : "");
+		}
+		os << "}";
+		return os;
+	}
+
+	void range_initialize(iterator first, iterator end)
+	{
+		//используется в реализация forward, чуть позже нужно доделать
+	}
+	void push_back(const T& value)
 	{
 		if (size_ == cap_)
 		{
 			reserve(cap_ > 0 ? cap_ * 2 : 1);
-			arr_[size_++] = tmp;
 		}
-		else
-		{
-			arr_[size_++] = tmp;
-		}
+		AllocTraits::construct(alloc_, arr_ + size_, value);
+		++size_;
 	}
-	void push_back(T&& tmp) noexcept
+	void push_back(T&& value)
 	{
 		if (size_ == cap_)
 		{
 			reserve(cap_ > 0 ? cap_ * 2 : 1);
-			arr_[size_] = std::move(tmp);
-			size_++;
 		}
-		else
-		{
-			arr_[size_ - 1] = std::move(tmp);
-			size_++;
-		}
+		AllocTraits::construct(alloc_, arr_ + size_, std::move(value));
+		++size_;
 	}
+
 	void reserve(size_t newcap)
 	{
 		if (newcap < cap_)
 			return;
 
 		T* newarr = AllocTraits::allocate(alloc_, newcap);//operator new();
-		size_t idx = 0;
+		size_t i = 0;
 		try
 		{
-			for (; idx < size_; ++idx)
+			for (; i < size_; ++i)
 			{
-				AllocTraits::construct(alloc_, newarr, std::move_if_noexcept(arr_[idx]));//new() ptr;
+				AllocTraits::construct(alloc_, newarr + i, std::move_if_noexcept(arr_[i]));//Если перемещение безопасно, то перемещаем
 			}
-
-			AllocTraits::deallocate(alloc_, newarr, newcap);
 		}
 		catch (...)
 		{
-			for (size_t old_idx = 0; old_idx < idx; ++old_idx)
+			for (size_t old_i = 0; old_i < i; ++old_i)
 			{
-				AllocTraits::destroy(alloc_, newarr + old_idx);
+				AllocTraits::destroy(alloc_, newarr + old_i);
 			}
+			AllocTraits::deallocate(alloc_, newarr, newcap);
+			throw;
 		}
 
 		for(size_t index = 0; index < size_; ++ index)
 		{
-			alloc_.deallocate(arr_, cap_);
+			AllocTraits::destroy(alloc_, arr_ + i);
 		}
+		AllocTraits::deallocate(alloc_, arr_, cap_);
 
 		arr_ = newarr;
-		alloc_.deallocate(arr_, cap_);
 		cap_ = newcap;
 	}//Используем доп память, пока без move семантики
+	
+	size_t size() const noexcept {return size_;}
+	size_t capacity() const noexcept{ return cap_; }
 	bool isEmpty()
 	{
 		return size_ == 0;
@@ -329,19 +447,27 @@ public:
 		AllocTraits::deallocate(alloc_, it);
 	}
 
-	iterator begin() { return iterator{ arr_ }; }
-	iterator end() {return iterator{ arr_ + size_ };}
+	iterator begin() noexcept { return iterator{ arr_ }; }
+	iterator end() noexcept {return iterator{ arr_ + size_ };}
 
-	const_iterator cbegin() const { return iterator{ arr_ }; }
-	const_iterator cend() const { return iterator{ arr_ + size_ }; }
+	const_iterator begin() const noexcept { return const_iterator{ arr_ }; }
+	const_iterator end() const noexcept { return const_iterator{ arr_ + size_ }; }
 
-	iterator rbegin() { return iterator{ arr_ + size_ }; }
-	iterator rend() { return iterator{ arr_ }; }
+	const_iterator cbegin() const noexcept { return const_iterator{ arr_ }; }
+	const_iterator cend() const noexcept { return const_iterator{ arr_ + size_ }; }
+
+	/*iterator rbegin() noexcept { return iterator{ arr_ + size_ }; }
+	iterator rend() noexcept { return iterator{ arr_ }; }*/
+	//Нужно использовать reverse iterator
 };
 
 
 int main()
 {
+	std::vector<int> stl_v;
 	vector<int> v;
 	v.push_back(5);
+	v.push_back(6);
+	v.reserve(10);
+	std::cout << v;
 }
