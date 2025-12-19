8324cbcc3d355479f1f292b225ce11e2d9764c63
Maxsilon1 <maxsilongusakov@gmail.com>
2025-12-19 19:46:35 +0300

    deque and list realiz

--- Zad5/Zad5.cpp
+++ Zad5/Zad5.cpp
@@ -239,130 +239,215 @@ public:
 template<class T, class Alloc = std::allocator<T>>
 class list
 {
+	// 1. Внутренние структуры
 	struct BaseNode
 	{
 		BaseNode* prev;
 		BaseNode* next;
 
-		BaseNode(): prev(nullptr), next(nullptr) {}
-		BaseNode(BaseNode* prev, BaseNode* next): prev(prev), next(next) {}
+		BaseNode() : prev(this), next(this) {} // Инициализация на себя для Sentinel
 	};
-	struct Node: BaseNode
+
+	struct Node : BaseNode
 	{
 		T value;
-
-		Node(): BaseNode() { value = 0; }
-		Node(T val) : BaseNode() { value = val; }
+		// Конструктор не нужен, будем использовать allocator::construct
 	};
-	
-	using AllocTraits = std::allocator_traits<NodeAlloc>;
-	using NodeAlloc = typename AllocTraits::template rebind_alloc<Node>;//Что это значит???
+
+public:
+	// 2. Стандартные using-объявления (Traits)
+	using value_type = T;
+	using allocator_type = Alloc;
+	using size_type = std::size_t;
+	using difference_type = std::ptrdiff_t;
+	using reference = value_type&;
+	using const_reference = const value_type&;
+	using pointer = typename std::allocator_traits<Alloc>::pointer;
+	using const_pointer = typename std::allocator_traits<Alloc>::const_pointer;
+
+private:
+	using AllocTraits = std::allocator_traits<Alloc>;
+	using NodeAlloc = typename AllocTraits::template rebind_alloc<Node>;
 	using NodeAllocTraits = std::allocator_traits<NodeAlloc>;
 
-	BaseNode* fakeNode_;
-	Node* node;
+	using BaseNodeAlloc = typename AllocTraits::template rebind_alloc<BaseNode>;
+	using BaseNodeAllocTraits = std::allocator_traits<BaseNodeAlloc>;
 
+	BaseNode* fakeNode_;
 	size_t size_;
 
-	Alloc alloc_;
-	NodeAlloc allocNode_;
+	Alloc alloc_; 
+	NodeAlloc nodeAlloc_;
+	BaseNodeAlloc baseNodeAlloc_;
+
+	void init()
+	{
+		// Выделяем память под фиктивный узел
+		fakeNode_ = BaseNodeAllocTraits::allocate(baseNodeAlloc_, 1);
+		// Конструируем BaseNode (устанавливает prev/next на себя)
+		BaseNodeAllocTraits::construct(baseNodeAlloc_, fakeNode_);
+		size_ = 0;
+	}
 
 	template<typename... Args>
-	void emplace_impl(Args... args, void* link(Node*))
+	void emplace_impl(BaseNode* pos, Args&&... args)
 	{
-		Node* newNode = NodeAllocTraits::allocate(allocNode_, 1);
+		Node* newNode = NodeAllocTraits::allocate(nodeAlloc_, 1);
 		try
 		{
-			newNode = AllocTraits::construct(alloc_, newNode, std::forward(args)...);//Используем конструктор нашего дефолтного аллокатора
+			AllocTraits::construct(alloc_, std::addressof(newNode->value), std::forward<Args>(args)...);
 
-			link(newNode);
+			newNode->next = pos;
+			newNode->prev = pos->prev;
+			pos->prev->next = newNode;
+			pos->prev = newNode;
+
 			++size_;
 		}
 		catch (...)
 		{
-			NodeAllocTraits::deallocate(allocNode_, newNode, 1);
+			NodeAllocTraits::deallocate(nodeAlloc_, newNode, 1);
+			throw;
 		}
 	}
 
-	void link_front(Node* newNode)
-	{
-		fakeNode_->next->prev = newNode; newNode->next = fakeNode_->prev;
-		fakeNode_->next = newNode;newNode->prev = fakeNode_;
-	}
-	void link_back(Node* newNode)
-	{
-		fakeNode_->prev->next = newNode; newNode->prev = fakeNode_->prev;
-		fakeNode_->prev = newNode;newNode->next = fakeNode_;
-	}
 public:
-	list(): fakeNode_{fakeNode_, fakeNode_}, size_(0) {}
-	explicit list(size_t sz) : fakeNode_{ fakeNode_, fakeNode_ }
+	list() : size_(0) 
+	{
+		init();
+	}
+	explicit list(size_t sz) : list()
 	{
 		for (size_t i = 0; i < sz; ++i)
-			push_back(T{});
-	};
-	explicit list(const list& other){}
-	
-	void push_back(const T& val)
-	{
-		emplace_impl(val, link_back);
-	}
-	void push_back(T&& val)
-	{
-		emplace_impl(std::move(val), link_back);
+			emplace_back(); // Создаст элементы по умолчанию
 	}
 
-	void push_front(const T& val)
+	list(const list& other) : list()
 	{
-		emplace_impl(val, link_front);
+		for (const auto& item : other)
+		{
+			push_back(item);
+		}
 	}
-	void push_front(T&& val)
+	list(list&& other) noexcept : list() // Делаем swap
 	{
-		emplace_impl(std::move(val), link_front);
+		swap(other);
 	}
 
-	bool empty() 
-	{ 
-		if (&fakeNode_ != &fakeNode_)return false; 
-		return true;
+	~list()
+	{
+		clear();
+		BaseNodeAllocTraits::destroy(baseNodeAlloc_, fakeNode_);
+		BaseNodeAllocTraits::deallocate(baseNodeAlloc_, fakeNode_, 1);
 	}
+
+	list& operator=(const list& other)
+	{
+		if (this == &other) return *this;
+		clear();
+		for (const auto& item : other)
+			push_back(item);
+		return *this;
+	}
+
+	list& operator=(list&& other) noexcept
+	{
+		if (this == &other) return *this;
+		clear();
+		swap(other);
+		return *this;
+	}
+
+	void swap(list& other) noexcept
+	{
+		std::swap(fakeNode_, other.fakeNode_);
+		std::swap(size_, other.size_);
+		std::swap(alloc_, other.alloc_);
+		std::swap(nodeAlloc_, other.nodeAlloc_);
+		std::swap(baseNodeAlloc_, other.baseNodeAlloc_);
+	}
+
+	template<typename... Args>
+	void emplace_back(Args&&... args)
+	{
+		emplace_impl(fakeNode_, std::forward<Args>(args)...);
+	}
+
+	template<typename... Args>
+	void emplace_front(Args&&... args)
+	{
+		emplace_impl(fakeNode_->next, std::forward<Args>(args)...);
+	}
+
+	void push_back(const T& val) { emplace_back(val); }
+	void push_back(T&& val) { emplace_back(std::move(val)); }
+
+	void push_front(const T& val) { emplace_front(val); }
+	void push_front(T&& val) { emplace_front(std::move(val)); }
+
+	bool empty() const { return size_ == 0; }
+	size_t size() const { return size_; }
 
 	void pop_back()
 	{
-
+		if (empty()) return;
+		erase(iterator(fakeNode_->prev));
 	}
+
 	void pop_front()
 	{
-
+		if (empty()) return;
+		erase(begin());
 	}
+
+	void clear()
+	{
+		while (!empty())
+		{
+			pop_back();
+		}
+	}
+
 private:
 	template<bool isConst>
 	class base_iterator
 	{
-	private:
+	public:
 		using iterator_category = std::bidirectional_iterator_tag;
-		using pointer = std::conditional_t<isConst, const T*, T*);
-		using reference = std::conditional_t<isConst, const T&, T&);
 		using value_type = T;
 		using difference_type = std::ptrdiff_t;
+		using pointer = std::conditional_t<isConst, const T*, T*>;
+		using reference = std::conditional_t<isConst, const T&, T&>;
 
-		Node* ptr;
+		// Внутри итератора храним BaseNode*, так как fakeNode_ это BaseNode
+		using NodePtr = BaseNode*;
+
+	private:
+		NodePtr ptr;
+
 	public:
-		base_iterator(const base_iterator&) = default;
-		base_iterator& operator=(const base_iterator&) = default;
+		base_iterator(NodePtr p) : ptr(p) {}
 
-		reference& operator*() const { return *ptr; }
-		pointer operator->() const { return ptr; }
-		pointer operator&() const { return ptr; }
+		template<bool wasConst, typename = std::enable_if_t<isConst && !wasConst>>
+		base_iterator(const base_iterator<wasConst>& other) : ptr(other.ptr) {}
+
+		reference operator*() const {
+			return static_cast<Node*>(ptr)->value; 
+		}
+
+		pointer operator->() const { 
+			return std::addressof(static_cast<Node*>(ptr)->value); 
+		}
 
 		base_iterator& operator++()
 		{
 			ptr = ptr->next;
 			return *this;
 		}
-		base_iterator& operator++(int)
+
+		base_iterator operator++(int)
 		{
-			base_iterator tmp = ptr;
+			base_iterator tmp = *this;
 			ptr = ptr->next;
 			return tmp;
 		}
@@ -372,57 +457,62 @@ private:
 			ptr = ptr->prev;
 			return *this;
 		}
-		base_iterator& operator--(int)
+
+		base_iterator operator--(int)
 		{
 			base_iterator tmp = *this;
 			ptr = ptr->prev;
 			return tmp;
 		}
 
-		base_iterator& operator+=(difference_type other)
-		{
-			return *this;
-		}
-		base_iterator& operator-=(difference_type other)
-		{
-			ptr -= other;
-			return *this;
-		}
+		bool operator==(const base_iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const base_iterator& other) const { return ptr != other.ptr; }
 
-		friend base_iterator& operator+(const base_iterator it, difference_type dif)
-		{
-			return it += dif;
-		}
-		friend base_iterator& operator+(difference_type dif, const base_iterator it)
-		{
-			return it += dif;
-		}
-
-		friend base_iterator& operator-(const base_iterator it, base_iterator& dif)
-		{
-			return it -= dif;
-		}
-		friend base_iterator& operator-(const base_iterator it, difference_type dif)
-		{
-			return it -= dif;
-		}
-		friend base_iterator& operator-(difference_type dif, const base_iterator it)
-		{
-			return it -= dif;
-		}
-
-		bool operator==(const base_iterator& other) { return ptr == ptr; }
-		bool operator!=(const base_iterator& other) { return ptr != ptr; }
-
-		bool operator<(const base_iterator& other) { return ptr < other.ptr; }
-		bool operator>(const base_iterator& other) { return ptr > other.ptr; }
-
-		bool operator<=(const base_iterator& other) { return ptr <= other.ptr; }
-		bool operator>=(const base_iterator& other) { return ptr >= other.ptr; }
+		friend class list<T, Alloc>;
 	};
+
 public:
 	using iterator = base_iterator<false>;
 	using const_iterator = base_iterator<true>;
+
+	iterator begin() { return iterator(fakeNode_->next); }
+	const_iterator begin() const { return const_iterator(fakeNode_->next); }
+	const_iterator cbegin() const { return const_iterator(fakeNode_->next); }
+
+	iterator end() { return iterator(fakeNode_); }
+	const_iterator end() const { return const_iterator(fakeNode_); }
+	const_iterator cend() const { return const_iterator(fakeNode_); }
+
+	iterator erase(iterator pos)
+	{
+		BaseNode* curr = pos.ptr;
+		BaseNode* nextNode = curr->next;
+		BaseNode* prevNode = curr->prev;
+
+		prevNode->next = nextNode;
+		nextNode->prev = prevNode;
+
+		Node* Node = static_cast<Node*>(curr);
+
+		AllocTraits::destroy(alloc_, std::addressof(Node->value));
+		NodeAllocTraits::deallocate(nodeAlloc_, Node, 1);
+
+		--size_;
+		return iterator(nextNode);
+	}
+
+	// Удаление по значению
+	void remove(const T& val)
+	{
+		iterator it = begin();
+		while (it != end())
+		{
+			if (*it == val)
+				it = erase(it); // erase возвращает следующий валидный итератор
+			else
+				++it;
+		}
+	}
 };
 
 template<class T, class Alloc = std::allocator<T>>
@@ -559,17 +649,17 @@ public:
 		size_ = other.size_;
 		cap_ = other.cap_;
 	}
-	
+
 	vector* operator+=(const T& value)
 	{
 		for (size_t i = 0; i < size_; ++i)
 			arr_[i] += value;
 		return *this;
 	}
-	vector& operator+=(const vector& other) 
+	vector& operator+=(const vector& other)
 	{
 		if (size_ != other.size_)throw std::length_error("bad sizes");
-		for (size_t i = 0; i < size_; ++i)
+		for (size_t i = 0; i < other.size_ && i < size_; ++i)
 			arr_[i] += other.arr_[i];
 		return *this;
 	}
@@ -580,7 +670,7 @@ public:
 			arr_[i] -= value;
 		return *this;
 	}
-	vector& operator-=(const vector& other) 
+	vector& operator-=(const vector& other)
 	{
 		if (size_ != other)throw std::length_error("bad sizes");
 		for (size_t i = 0; i < size_; ++i)
@@ -596,7 +686,7 @@ public:
 	{
 		return first += second;
 	}
-	friend vector& operator+(T& first, vector second) 
+	friend vector& operator+(T& first, vector second)
 	{
 		return second += first;
 	}
@@ -607,7 +697,7 @@ public:
 	}
 	friend vector& operator-(vector first, const T& second) { return first -= second; }
 	friend vector& operator-(const T& first, vector second) { return second -= first; }
-	
+
 	friend std::ostream& operator<<(std::ostream& os, const vector& v)//У std::ostream - удален конструктор копирования, можно вызывать только по ссылке
 	{
 		os << "{";//Вводим в поток os все наши данные
@@ -640,6 +730,13 @@ public:
 		}
 		AllocTraits::construct(alloc_, arr_ + size_, std::move(value));
 		++size_;
+	}
+	void push_back(std::initializer_list<T> ilist)
+	{
+		for (auto i : ilist)
+		{
+			push_back(i);
+		}
 	}
 
 	void reserve(size_t newcap)
@@ -720,45 +817,233 @@ class deque
 public:
 	using value_type = T;
 	using reference = T&;
+	using const_reference = const T&;
 	using pointer = T*;
+	using size_type = size_t;
+
 private:
-	pointer* arr;//Будет хранить указатели на массивы
+	using AllocTraits = std::allocator_traits<Alloc>;
 
-	size_t head;
-	size_t tail;
+	T* arr_;          // Один сплошной массив
+	size_t cap_;      // Вместимость массива
+	size_t head_;     // Индекс начала данных
+	size_t size_;     // Текущее количество элементов
 
-	size_t size_;
-	size_t default_arr_size_ = 30;
-	
 	Alloc alloc_;
 
-	using AllocTraits = std::allocator_traits<Alloc>;
-public:
-	deque(): arr(nullptr), head(0), tail(0), size_(0) {}
-	deque(size_t sz): arr(nullptr), head(0), tail(0), size_(0){}
-
-	void push_back()
+	// Получение физического индекса в массиве по логическому индексу (0...size-1)
+	size_t get_physical_index(size_t index) const
 	{
-		if(tail % default_arr_size_== default_arr_size_ - 1)
+		return (head_ + index) % cap_;
+	}
+
+	// Увеличение массива
+	void grow()
+	{
+		size_t new_cap = (cap_ == 0) ? 8 : cap_ * 2;
+		T* new_arr = AllocTraits::allocate(alloc_, new_cap);
+
+		try
 		{
-			resize(size_);//Увеличиваем количество подмассивов, которые будут распределяться между элементами
+			// Переносим элементы в новый массив. 
+			// В новом массиве выстраиваем их линейно, начиная с индекса 0.
+			for (size_t i = 0; i < size_; ++i)
+			{
+				size_t old_idx = get_physical_index(i);
+				// Перемещаем элемент (move construct)
+				AllocTraits::construct(alloc_, new_arr + i, std::move(arr_[old_idx])); 
+			}
+		}
+		catch (...)
+		{
+			// Если упало, чистим то, что успели создать
+			// (упрощенная очистка для примера)
+			AllocTraits::deallocate(alloc_, new_arr, new_cap);
+			throw;
+		}
+
+		// Удаляем старые элементы и старый массив
+		if (arr_)
+		{
+			for (size_t i = 0; i < size_; ++i)
+				AllocTraits::destroy(alloc_, arr_ + get_physical_index(i));
+			AllocTraits::deallocate(alloc_, arr_, cap_);
+		}
+
+		arr_ = new_arr;
+		cap_ = new_cap;
+		head_ = 0; // Теперь данные начинаются с 0
+		// size_ остается прежним
+	}
+
+public:
+	deque() : arr_(nullptr), cap_(0), head_(0), size_(0) {}
+
+	explicit deque(size_t sz) : deque()
+	{
+		if (sz > 0)
+		{
+			cap_ = sz;
+			arr_ = AllocTraits::allocate(alloc_, cap_);
+			for (size_t i = 0; i < sz; ++i)
+				AllocTraits::construct(alloc_, arr_ + i); // Default construct
+			size_ = sz;
 		}
 	}
-	void push_front()
-	{
-		
-	}
-	void resize()
-	{
 
-	}
-	void reserve(size_t sz)
+	~deque()
 	{
-
+		clear();
+		if (arr_)
+			AllocTraits::deallocate(alloc_, arr_, cap_);
 	}
+
+	// --- Добавление элементов ---
+
+	template<typename... Args>
+	void emplace_back(Args&&... args)
+	{
+		if (size_ == cap_)
+			grow();
+
+		// tail = (head + size) % cap
+		size_t tail = get_physical_index(size_);
+
+		AllocTraits::construct(alloc_, arr_ + tail, std::forward<Args>(args)...);
+		++size_;
+	}
+
+	void push_back(const T& val)
+	{
+		emplace_back(val);
+	}
+
+	void push_back(T&& val)
+	{
+		emplace_back(std::move(val));
+	}
+
+	template<typename... Args>
+	void emplace_front(Args&&... args)
+	{
+		if (size_ == cap_)
+			grow();
+
+		// head движется назад циклически
+		// (head - 1 + cap) % cap
+		head_ = (head_ == 0) ? cap_ - 1 : head_ - 1;
+
+		try {
+			AllocTraits::construct(alloc_, arr_ + head_, std::forward<Args>(args)...);
+		} catch (...) {
+			// Возвращаем head обратно, если конструирование упало
+			head_ = (head_ + 1) % cap_;
+			throw;
+		}
+		++size_;
+	}
+
+	void push_front(const T& val)
+	{
+		emplace_front(val);
+	}
+
+	void push_front(T&& val)
+	{
+		emplace_front(std::move(val));
+	}
+
+	// --- Удаление ---
+
+	void pop_back()
+	{
+		if (empty()) return;
+
+		size_t tail = get_physical_index(size_ - 1);
+		AllocTraits::destroy(alloc_, arr_ + tail);
+		--size_;
+	}
+
+	void pop_front()
+	{
+		if (empty()) return;
+
+		AllocTraits::destroy(alloc_, arr_ + head_);
+		head_ = (head_ + 1) % cap_;
+		--size_;
+	}
+
+	void clear()
+	{
+		while (!empty())
+			pop_back();
+	}
+
+	// --- Доступ и свойства ---
+
+	bool empty() const { return size_ == 0; }
+	size_t size() const { return size_; }
+
+	reference operator[](size_t idx)
+	{
+		return arr_[get_physical_index(idx)];
+	}
+
+	const_reference operator[](size_t idx) const
+	{
+		return arr_[get_physical_index(idx)];
+	}
+
+	// --- Итератор ---
+	// Итератор для кольцевого буфера сложнее обычного указателя,
+	// так как память физически может "разрываться" (конец массива -> начало массива).
+	template<bool IsConst>
+	struct DequeIterator
+	{
+		using pointer = std::conditional_t<IsConst, const T*, T*>;
+		using reference = std::conditional_t<IsConst, const T&, T&>;
+		using value_type = T;
+		using iterator_category = std::random_access_iterator_tag;
+		using difference_type = std::ptrdiff_t;
+
+		// Храним указатель на сам дек и логический индекс
+		using DequePtr = std::conditional_t<IsConst, const deque*, deque*>;
+		DequePtr deq_ptr;
+		size_t idx;
+
+		DequeIterator(DequePtr d, size_t i) : deq_ptr(d), idx(i) {}
+
+		reference operator*() const { return (*deq_ptr)[idx]; }
+		pointer operator->() const { return &(*deq_ptr)[idx]; }
+
+		DequeIterator& operator++() { ++idx; return *this; }
+		DequeIterator operator++(int) { DequeIterator tmp = *this; ++idx; return tmp; }
+		DequeIterator& operator--() { --idx; return *this; }
+		DequeIterator operator--(int) { DequeIterator tmp = *this; --idx; return tmp; }
+
+		DequeIterator& operator+=(difference_type n) { idx += n; return *this; }
+		DequeIterator& operator-=(difference_type n) { idx -= n; return *this; }
+
+		difference_type operator-(const DequeIterator& other) const { return idx - other.idx; }
+		DequeIterator operator+(difference_type n) const { return { deq_ptr, idx + n }; }
+		DequeIterator operator-(difference_type n) const { return { deq_ptr, idx - n }; }
+
+		bool operator==(const DequeIterator& other) const { return idx == other.idx; }
+		bool operator!=(const DequeIterator& other) const { return idx != other.idx; }
+		bool operator<(const DequeIterator& other) const { return idx < other.idx; }
+	};
+
+	using iterator = DequeIterator<false>;
+	using const_iterator = DequeIterator<true>;
+
+	iterator begin() { return iterator(this, 0); }
+	iterator end() { return iterator(this, size_); }
+
+	const_iterator begin() const { return const_iterator(this, 0); }
+	const_iterator end() const { return const_iterator(this, size_); }
 };
 
 int main()
 {
-	std::list<int> l;
+	list<int> l; l.push_front(10);
 }
